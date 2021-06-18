00965600b66f4e4b0a1bdc825c9b7c4fd2572f45
Abel <eduardo.9666@hotmail.com>
2021-06-18 12:23:57 -0500

    first commit mf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+// importo clase creada en ./persona.model.ts
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'listado-personas';
+  // variable title de tipo string
+  title = 'Listado de personas';
+  
+  // variable personas de tipo Persona
+  personas: Persona[]=[
+    new Persona('Juan','Perez'), 
+    new Persona('Laura', 'Juarez'),
+    new Persona('Carla', 'Lara')   
+  ];
 }
